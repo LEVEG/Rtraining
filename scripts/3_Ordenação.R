34444420d0a4a51b951b9c67a63e23201829c3b2
jklipel <klipeljoice@gmail.com>
2021-08-20 09:46:33 -0300

    PCA-Traits

--- scripts/3_Ordenação.R
+++ scripts/3_Ordenação.R
@@ -16,13 +16,6 @@ pca_co$x
 # Figura
 biplot(pca_co)
 
-## carregar tabela
-#primeiro abrir diretório da pasta de dados
-ord.var_co <- read.table("PCA_coocorrem.txt", h=T)
-head(ord.var_co)
-dados.pca_co<-ord.var_co[,(-1)];dados.pca_co
-#class(dados.pca_co$SM)
-
 # Figura bonitinha
 library(ggfortify)
 ordpca_co<-autoplot(pca_co, 
@@ -49,6 +42,48 @@ ordpca_co<-autoplot(pca_co,
         legend.title = element_text(size=rel(1.1))) +
   labs(color="Strata")
 ordpca_co
+
+
+## pca
+
+###### esp?cies que coocorrem
+ord.var_co <- read.table("PCA_coocorrem.txt", h=T)
+head(ord.var_co)
+dados.pca_co<-ord.var_co[,(-1)]
+pca_co <- prcomp(dados.pca_co, center = T, scale. = T)
+pca_co
+biplot(pca_co)
+my.theme<-theme(axis.title=element_text(size=rel(1.3)), axis.text=element_text(size=rel(1.3)), 
+                axis.line = element_line(colour = "black"), 
+                panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+                panel.border=element_rect(fill=NA, size=1, color="black"),
+                panel.background = element_blank())
+ordpca_co<-autoplot(pca_co, 
+                    label=FALSE, 
+                    data=ord.var_co, #dados originais inseridos na pca
+                    colour="trat", #coluna que indica os grupos de cores
+                    label.size=4, #tamanho dos rotulos
+                    shape=19, #omite o simbolo
+                    size=3,
+                    loadings=TRUE, #inclui as setas com as variaveis
+                    loadings.colour="black" #edita a cor das setas
+) +
+  theme_bw() +my.theme +
+  scale_color_manual(values = c("#666666", "#996600")) +
+  geom_vline(xintercept=0, color="black", linetype="dotted") +
+  geom_hline(yintercept=0, color="black", linetype="dotted") +
+  annotate("text", x=-0.15, y=-0.3, label= "LA") +
+  annotate("text", x=0.4, y=0.15, label= "LDMC") +
+  annotate("text", x=-0.10, y=0.38, label= "SM") +
+  annotate("text", x=-0.25, y=0.30, label= "SN") +
+  annotate("text", x=-0.33, y=-0.03, label= "SLA") +
+  theme(legend.background=element_rect(color="grey"),
+        legend.position=c(0.12,0.90),
+        legend.text=element_text(size=rel(1.1)),
+        legend.title = element_text(size=rel(1.1))) +
+  labs(color="Strata")
+ordpca_co
+
 
 ## PCoA ------------
 d <- vegdist(iris[,1:4])
