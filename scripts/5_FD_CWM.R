70c3f4eff4da75838b4363f20559374cd320b936
jklipel <klipeljoice@gmail.com>
2022-05-24 15:41:52 -0300

    #função lab

--- scripts/5_FD_CWM.R
+++ scripts/5_FD_CWM.R
@@ -6,3 +6,9 @@ ex1 <- dbFD(dummy$trait, dummy$abun)
 ex1
 
 # importante: nome das especies devem estar iguais e na mesma ordem!
+
+comm <- read.table("comm_leveg.txt", h=T, row.names = 1); head(comm)
+traits <- read.table("atributos.txt", h=T, row.names = 1); head(traits)
+
+teste <- dbFD(traits, comm)
+teste
